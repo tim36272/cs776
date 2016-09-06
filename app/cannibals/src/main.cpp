75ca61d0b081d1b0853518f2fea63ca89482fd86
Tim Sweet <timothy.l.sweet@gmail.com>
2016-09-05 20:55:10 -0700

    Add comments and remove unneeded ionalgolib

--- app/cannibals/src/main.cpp
+++ app/cannibals/src/main.cpp
@@ -1,20 +1,47 @@
+/*
+Copyright (C) 2016  Tim Sweet
+
+This program is free software : you can redistribute it and / or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.If not, see <http://www.gnu.org/licenses/>.
+
+*/
 #include "ionlib\log.h"
-#include "ionlib\net.h"
 #include "ionlib\tree.h"
+#include "ionlib\math.h"
 #include <vector>
 #include <fstream>
 #include <sstream>
 
-#define BOAT_CAPACITY 2
-#define NUM_MISSIONARIES 3
-#define NUM_CANNIBALS 3
+//This is used to get command line paramters
+typedef struct RiverConfig_e {
+	uint32_t num_missionaries;
+	uint32_t num_cannibals;
+	uint32_t boat_capacity;
+} RiverConfig;
+
+/*
+RiverState defines one configuration of the missionaries, cannibals, and the boat
+
+Its primary purpose is just to store the member variables, and includes some
+other required operators
+*/
 class RiverState
 {
 public:
 	//the convention is that the fields below answer the question "is the item on the correct side of the river?",
 	//that is, the initial state is false and the final state is true
-	std::vector<bool> missionary_state;
-	std::vector<bool> cannibal_state;
+	std::vector<bool> missionaries;
+	std::vector<bool> cannibals;
 	bool boat_state;
 	bool operator==(const RiverState& rhs) const
 	{
@@ -22,37 +49,48 @@ public:
 		{
 			return false;
 		}
-		size_t num_missionaries_lhs = std::count(this->missionary_state.begin(), this->missionary_state.end(), false);
-		size_t num_canibals_lhs = std::count(this->cannibal_state.begin(), this->cannibal_state.end(), false);
-		size_t num_missionaries_rhs = std::count(rhs.missionary_state.begin(), rhs.missionary_state.end(), false);
-		size_t num_canibals_rhs = std::count(rhs.cannibal_state.begin(), rhs.cannibal_state.end(), false);
+		size_t num_missionaries_lhs = std::count(this->missionaries.begin(), this->missionaries.end(), false);
+		size_t num_canibals_lhs = std::count(this->cannibals.begin(), this->cannibals.end(), false);
+		size_t num_missionaries_rhs = std::count(rhs.missionaries.begin(), rhs.missionaries.end(), false);
+		size_t num_canibals_rhs = std::count(rhs.cannibals.begin(), rhs.cannibals.end(), false);
 		return (num_missionaries_lhs == num_missionaries_rhs && num_canibals_lhs == num_canibals_rhs);
 	}
 	friend std::ostream& operator<<(std::ostream& output, RiverState state)
 	{
 		output << "<";
-		for (std::vector<bool>::iterator missionary = state.missionary_state.begin(); missionary != state.missionary_state.end(); ++missionary)
+		for (std::vector<bool>::iterator missionary = state.missionaries.begin(); missionary != state.missionaries.end(); ++missionary)
 		{
-			output << *missionary << ",";
+			output << *missionary;
+			if (missionary + 1 != state.missionaries.end())
+			{
+				output << ",";
+			}
 		}
-		for (std::vector<bool>::iterator cannibal = state.cannibal_state.begin(); cannibal != state.cannibal_state.end(); ++cannibal)
+		output << "><";
+		for (std::vector<bool>::iterator cannibal = state.cannibals.begin(); cannibal != state.cannibals.end(); ++cannibal)
 		{
-			output << *cannibal << ",";
+			output << *cannibal;
+			if (cannibal + 1 != state.cannibals.end())
+			{
+				output << ",";
+			}
 		}
-		output << state.boat_state;
-		output << ">";
+		output << "><" << state.boat_state << ">";
 		return output;
 	}
 };
-RiverState movePeople(RiverState state, size_t num_missionaries, size_t num_cannibals)
+//This function updates state by moving a number of people to the other side of
+//the river. Notice that state includes the position of the boat, so "move" is
+//not ambiguous
+RiverState MovePeople(RiverState state, size_t num_missionaries, size_t num_cannibals)
 {
 	size_t index = 0;
 	while (num_missionaries > 0)
 	{
-		LOGASSERT(index < state.missionary_state.size(), "Attempted to move more missionaries than are available");
-		if (state.missionary_state[index] == state.boat_state)
+		LOGASSERT(index < state.missionaries.size(), "Attempted to move more missionaries than are available");
+		if (state.missionaries[index] == state.boat_state)
 		{
-			state.missionary_state[index] = !state.missionary_state[index];
+			state.missionaries[index] = !state.missionaries[index];
 			num_missionaries--;
 		}
 		++index;
@@ -60,10 +98,10 @@ RiverState movePeople(RiverState state, size_t num_missionaries, size_t num_cann
 	index = 0;
 	while (num_cannibals > 0)
 	{
-		LOGASSERT(index < state.cannibal_state.size());
-		if (state.cannibal_state[index] == state.boat_state)
+		LOGASSERT(index < state.cannibals.size(), "Attempted to move more cannibals than are available");
+		if (state.cannibals[index] == state.boat_state)
 		{
-			state.cannibal_state[index] = !state.cannibal_state[index];
+			state.cannibals[index] = !state.cannibals[index];
 			num_cannibals--;
 		}
 		++index;
@@ -71,12 +109,14 @@ RiverState movePeople(RiverState state, size_t num_missionaries, size_t num_cann
 	state.boat_state = !state.boat_state;
 	return state;
 }
+//This function checks if the cannibals outnumber the missionaries on either
+//side of the river
 bool IsStateValid(RiverState state)
 {
-	size_t num_missionaries_origin = std::count(state.missionary_state.begin(), state.missionary_state.end(), false);
-	size_t num_canibals_origin = std::count(state.cannibal_state.begin(), state.cannibal_state.end(), false);
-	size_t num_missionaries_dest = std::count(state.missionary_state.begin(), state.missionary_state.end(), true);
-	size_t num_canibals_dest = std::count(state.cannibal_state.begin(), state.cannibal_state.end(), true);
+	size_t num_missionaries_origin = std::count(state.missionaries.begin(), state.missionaries.end(), false);
+	size_t num_canibals_origin = std::count(state.cannibals.begin(), state.cannibals.end(), false);
+	size_t num_missionaries_dest = std::count(state.missionaries.begin(), state.missionaries.end(), true);
+	size_t num_canibals_dest = std::count(state.cannibals.begin(), state.cannibals.end(), true);
 
 	bool result = true;
 	if (num_missionaries_origin > 0)
@@ -95,21 +135,44 @@ bool IsStateValid(RiverState state)
 	}
 	return result;
 }
-void enumerateAllStates(ion::TreeNode<RiverState>& root, ion::TreeNode<RiverState>* node)
+/*
+  This function generates a tree of all possible, non-cyclic, valid (i.e. meeting
+  the cannibal criteria) states.
+
+  Notice that it does not stop at the goal. This is just so that the complete
+  state tree can be printed at the end. It could easily be optimized to stop at
+  the goal.
+
+  The basic algorithm here is:
+  1. Start at the root
+  2. Generate every possible set of moves given:
+	* The number of people on the boat's side of the river
+	* The boat's capacity
+	* The cannibal criteria
+  3. For each valid state found, push it into the tree and into a queue
+	 (implemented as a vector because why not)
+  4. For each element in the queue, recursively call this function
+
+  Note that the function is recursive and doesn't implement any depth-checking.
+  It is conceivable that this could recurse forever, but it shouldn't (i.e. I
+  tested it)
+*/
+void enumerateAllStates(ion::TreeNode<RiverState>& root, ion::TreeNode<RiverState>* node, RiverConfig river_config)
 {
-	LOGDEBUG("Descending");
-	//get the number of cannibals/missionaries that could possibly be moved
-	//cap the number of movable people at BOAT_CAPACITY
 	RiverState state = node->GetData();
-	size_t num_missionaries_movable = min(BOAT_CAPACITY,std::count(state.missionary_state.begin(), state.missionary_state.end(), state.boat_state));
-	size_t num_cannibals_movable = min(BOAT_CAPACITY, std::count(state.cannibal_state.begin(), state.cannibal_state.end(), state.boat_state));
+	//get the number of cannibals/missionaries that could possibly be moved
+	//cap the number of movable people at river_config.boat_capacity
+	size_t num_missionaries_movable = min(river_config.boat_capacity,std::count(state.missionaries.begin(), state.missionaries.end(), state.boat_state));
+	size_t num_cannibals_movable = min(river_config.boat_capacity, std::count(state.cannibals.begin(), state.cannibals.end(), state.boat_state));
+	
+	//This vector stores all the nodes waiting to be expanded
 	std::vector<ion::TreeNode<RiverState>*> pending_nodes;
 
-	//for node, generate all of its children.
+	//Generate all of this node's children.
 	for (size_t num_missionaries_moved = 0; num_missionaries_moved <= num_missionaries_movable; ++num_missionaries_moved)
 	{
 		size_t max_cannibals_to_move;
-		max_cannibals_to_move = (num_cannibals_movable < num_missionaries_moved) ? 0 : (num_cannibals_movable - num_missionaries_moved);
+		max_cannibals_to_move = min(river_config.boat_capacity - num_missionaries_moved, num_cannibals_movable);
 		for (size_t num_cannibals_moved = 0; num_cannibals_moved <= max_cannibals_to_move; ++num_cannibals_moved)
 		{
 			//check if there is no one to drive the boat, if so skip this state
@@ -117,94 +180,121 @@ void enumerateAllStates(ion::TreeNode<RiverState>& root, ion::TreeNode<RiverStat
 			{
 				continue;
 			}
-			//note that the conditions on getting to this point in the loop guarantees that there are enough people to move and the boat will fit this many people
-			//so we can just generate the state and check if it is in the tree already
-			RiverState new_state = movePeople(state, num_missionaries_moved, num_cannibals_moved);
+			//Generate the transition from this state to the next
+			RiverState new_state = MovePeople(state, num_missionaries_moved, num_cannibals_moved);
 			//check if the new_state is valid (i.e. cannibals don't outnumber missionaries)
 			bool valid_state = IsStateValid(new_state);
 			if (!valid_state)
 			{
 				continue;
 			}
-			//check if this state is already in the tree
+			/*
+				Note that the conditions on getting to this point in the loop
+				guarantees that:
+				  * There are enough people to move
+				  * The boat will fit this many people
+				  * The state is valid
+				So we can just check if it is in the tree already and add it
+				
+				The next function checks if this state is already in the tree.
+
+				Note the GetPath function returns an empty vector if there is no
+				path to this state. So, we don't actually need the path, just
+				its length
+			*/
 			std::vector<ion::TreeNode<RiverState>*> path = root.GetPath(new_state);
 			if (path.empty())
 			{
 				//there is no path in the tree which leads to an equivalent state, which means this state is novel
 				node->AddLeaf(new_state);
-				//enumerate all the children of that state
+				//push all the children of that state
 				pending_nodes.push_back(node->GetLeaf(node->NumLeafs() - 1));
 				
 			}
 		}
 	}
+	/*
+		The pending_nodes vector now contains all of the child nodes we pushed
+		in this function. Recursively calling this function on each node in
+		pending_nodes causes us to do a breadth-first search
+	*/
 	for (std::vector<ion::TreeNode<RiverState>*>::iterator node_it = pending_nodes.begin(); node_it != pending_nodes.end(); ++node_it)
 	{
-		enumerateAllStates(root, *node_it);
+		enumerateAllStates(root, *node_it, river_config);
 	}
-	LOGDEBUG("Ascending");
 }
 int main(int argc, char* argv[])
 {
 	/*
 	  The ion library is my (Tim Sweet's) general-purpose set of utility
-	  functions, such as printing, sockets, etc. I am justing starting to write
-	  it, so all of the core functionality for this assignment (i.e. trees,
-	  traversal, etc.) has been implemented from scratch just for this
-	  assignment. See my commit log at https://github.com/tim36272/ion for
-	  proof (in the commit log).
+	  functions, such as logging, sockets, etc. All of the core functionality
+	  for this assignment (i.e. trees, traversal, etc.) has been implemented
+	  from scratch just for this assignment. See my commit log at
+	  https://github.com/tim36272/ion for proof (in the commit log).
 	*/
-	ion::LogInit("app");
+	ion::LogInit("cannibals.log");
+
+	if (argc < 4)
+	{
+		LOGFATAL("Usage: main.exe num_missionaries num_cannibals boat_capacity");
+	}
+	//get the number of missionaries, cannibals, and boat capacity
+	RiverConfig river_config;
+	river_config.num_missionaries = (uint32_t)atoi(argv[1]);
+	river_config.num_cannibals = (uint32_t)atoi(argv[2]);
+	river_config.boat_capacity = (uint32_t)atoi(argv[3]);
 
 	//open a file to write the results to
 	std::ofstream fout;
-	std::stringstream filename;
-	filename << NUM_MISSIONARIES << "_missionaries_" << NUM_CANNIBALS << "_cannibals_" << BOAT_CAPACITY << "_seats.txt";
-	fout.open(filename.str());
-	fout << filename.str() << std::endl;
+	std::stringstream result_filename;
+	result_filename << river_config.num_missionaries << "missionaries_" << river_config.num_cannibals << "cannibals_" << river_config.boat_capacity << "seats.txt";
+	fout.open(result_filename.str());
+	fout << result_filename.str() << std::endl;
 
 	//Setup the problem with the requested number of actors
 	//Note the convention for true/false is the answer the question
 	//	"Is the item on the correct side of the river?"
 	RiverState initial_state;
 	initial_state.boat_state = false;
-	initial_state.cannibal_state.resize(NUM_CANNIBALS, false);
-	initial_state.missionary_state.resize(NUM_MISSIONARIES, false);
+	initial_state.cannibals.resize(river_config.num_cannibals, false);
+	initial_state.missionaries.resize(river_config.num_missionaries, false);
 
 	//Create the head of the tree
 	ion::TreeNode<RiverState> tree(initial_state,nullptr);
 
 	/*
-	  This function generates a tree of the entire valid state space without
-	  cycles.
+		This function generates a tree of the entire valid state space without
+		cycles.
 
-	  Thus it does not care when it reaches the goal node, it just exhaustively
-	  searches the space.
+		Thus it does not care when it reaches the goal node, it just exhaustively
+		searches the space.
 
-	  Of course this could be optimized to stop as soon as it reaches the goal,
-	  but for the purpose of fulfilling question one of the assignment ("Draw a
-	  diagram of the complete state space") it is necessary to search to
-	  exhaustion
+		Of course this could be optimized to stop as soon as it reaches the goal,
+		but for the purpose of fulfilling question one of the assignment ("Draw a
+		diagram of the complete state space") it is necessary to search to
+		exhaustion
 	*/
-	enumerateAllStates(tree, &tree);
+	enumerateAllStates(tree, &tree, river_config);
 	
 	//This prints the complete map of the valid state space.
 	std::ofstream file;
-	file.open("tree.gv");
+	std::stringstream map_filename;
+	map_filename << river_config.num_missionaries << "missionaries_" << river_config.num_cannibals << "cannibals_" << river_config.boat_capacity << "seats.gv";
+	file.open(map_filename.str());
 	tree.print(file);
 	file.close();
 
 	//Generate the goal node so we can search for it
 	RiverState goal_state;
 	goal_state.boat_state = true;
-	goal_state.cannibal_state.resize(NUM_CANNIBALS, true);
-	goal_state.missionary_state.resize(NUM_CANNIBALS, true);
+	goal_state.cannibals.resize(river_config.num_cannibals, true);
+	goal_state.missionaries.resize(river_config.num_missionaries, true);
 
 	/*
-	  This function finds the goal node in the tree. Note that it doesn't
-	  actually matter which way I search I the graph here because it has
-	  already been generated without cycles, that is, the goal node is unique
-	  in the already-built graph.
+		This function finds the goal node in the tree. Note that it doesn't
+		actually matter which way I search I the graph here because it has
+		already been generated without cycles, that is, the goal node is unique
+		in the already-built graph.
 	*/
 
 	ion::TreeNode<RiverState>::iterator it(ion::BREADTH_FIRST);
