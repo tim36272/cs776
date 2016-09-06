d8527e1dcdd0152a7e6d7afb38a3ea89d6a9274f
Tim Sweet <timothy.l.sweet@gmail.com>
2016-09-05 19:42:22 -0700

    Update to find the goal node and print to file, and add lots of comments

--- app/cannibals/src/main.cpp
+++ app/cannibals/src/main.cpp
@@ -3,6 +3,7 @@
 #include "ionlib\tree.h"
 #include <vector>
 #include <fstream>
+#include <sstream>
 
 #define BOAT_CAPACITY 2
 #define NUM_MISSIONARIES 3
@@ -70,6 +71,30 @@ RiverState movePeople(RiverState state, size_t num_missionaries, size_t num_cann
 	state.boat_state = !state.boat_state;
 	return state;
 }
+bool IsStateValid(RiverState state)
+{
+	size_t num_missionaries_origin = std::count(state.missionary_state.begin(), state.missionary_state.end(), false);
+	size_t num_canibals_origin = std::count(state.cannibal_state.begin(), state.cannibal_state.end(), false);
+	size_t num_missionaries_dest = std::count(state.missionary_state.begin(), state.missionary_state.end(), true);
+	size_t num_canibals_dest = std::count(state.cannibal_state.begin(), state.cannibal_state.end(), true);
+
+	bool result = true;
+	if (num_missionaries_origin > 0)
+	{
+		if (num_missionaries_origin < num_canibals_origin)
+		{
+			result = false;
+		}
+	}
+	if (num_missionaries_dest > 0)
+	{
+		if (num_missionaries_dest < num_canibals_dest)
+		{
+			result = false;
+		}
+	}
+	return result;
+}
 void enumerateAllStates(ion::TreeNode<RiverState>& root, ion::TreeNode<RiverState>* node)
 {
 	LOGDEBUG("Descending");
@@ -88,10 +113,19 @@ void enumerateAllStates(ion::TreeNode<RiverState>& root, ion::TreeNode<RiverStat
 		for (size_t num_cannibals_moved = 0; num_cannibals_moved <= max_cannibals_to_move; ++num_cannibals_moved)
 		{
 			//check if there is no one to drive the boat, if so skip this state
-			if (num_cannibals_moved == 0 && num_missionaries_moved == 0) continue;
+			if (num_cannibals_moved == 0 && num_missionaries_moved == 0)
+			{
+				continue;
+			}
 			//note that the conditions on getting to this point in the loop guarantees that there are enough people to move and the boat will fit this many people
 			//so we can just generate the state and check if it is in the tree already
 			RiverState new_state = movePeople(state, num_missionaries_moved, num_cannibals_moved);
+			//check if the new_state is valid (i.e. cannibals don't outnumber missionaries)
+			bool valid_state = IsStateValid(new_state);
+			if (!valid_state)
+			{
+				continue;
+			}
 			//check if this state is already in the tree
 			std::vector<ion::TreeNode<RiverState>*> path = root.GetPath(new_state);
 			if (path.empty())
@@ -112,29 +146,101 @@ void enumerateAllStates(ion::TreeNode<RiverState>& root, ion::TreeNode<RiverStat
 }
 int main(int argc, char* argv[])
 {
-	ion::InitSockets();
+	/*
+	  The ion library is my (Tim Sweet's) general-purpose set of utility
+	  functions, such as printing, sockets, etc. I am justing starting to write
+	  it, so all of the core functionality for this assignment (i.e. trees,
+	  traversal, etc.) has been implemented from scratch just for this
+	  assignment. See my commit log at https://github.com/tim36272/ion for
+	  proof (in the commit log).
+	*/
 	ion::LogInit("app");
 
+	//open a file to write the results to
+	std::ofstream fout;
+	std::stringstream filename;
+	filename << NUM_MISSIONARIES << "_missionaries_" << NUM_CANNIBALS << "_cannibals_" << BOAT_CAPACITY << "_seats.txt";
+	fout.open(filename.str());
+	fout << filename.str() << std::endl;
+
+	//Setup the problem with the requested number of actors
+	//Note the convention for true/false is the answer the question
+	//	"Is the item on the correct side of the river?"
 	RiverState initial_state;
 	initial_state.boat_state = false;
 	initial_state.cannibal_state.resize(NUM_CANNIBALS, false);
 	initial_state.missionary_state.resize(NUM_MISSIONARIES, false);
 
+	//Create the head of the tree
 	ion::TreeNode<RiverState> tree(initial_state,nullptr);
+
+	/*
+	  This function generates a tree of the entire valid state space without
+	  cycles.
+
+	  Thus it does not care when it reaches the goal node, it just exhaustively
+	  searches the space.
+
+	  Of course this could be optimized to stop as soon as it reaches the goal,
+	  but for the purpose of fulfilling question one of the assignment ("Draw a
+	  diagram of the complete state space") it is necessary to search to
+	  exhaustion
+	*/
 	enumerateAllStates(tree, &tree);
 	
+	//This prints the complete map of the valid state space.
 	std::ofstream file;
 	file.open("tree.gv");
 	tree.print(file);
 	file.close();
 
-	//ion::TreeNode<int>::iterator it(ion::DEPTH_FIRST);
-	//it.init(tree);
-	////ion::algo::traverse(ion::algo::BREADTH_FIRST, tree);
-	//while (!it.complete())
-	//{
-	//	LOGINFO("Tree data: %d", (*it).GetData());
-	//	++it;
-	//}
+	//Generate the goal node so we can search for it
+	RiverState goal_state;
+	goal_state.boat_state = true;
+	goal_state.cannibal_state.resize(NUM_CANNIBALS, true);
+	goal_state.missionary_state.resize(NUM_CANNIBALS, true);
+
+	/*
+	  This function finds the goal node in the tree. Note that it doesn't
+	  actually matter which way I search I the graph here because it has
+	  already been generated without cycles, that is, the goal node is unique
+	  in the already-built graph.
+	*/
+
+	ion::TreeNode<RiverState>::iterator it(ion::BREADTH_FIRST);
+	it.init(&tree);
+	while (!it.complete())
+	{
+		if ((*it)->GetData() == goal_state)
+		{
+			//We have found the goal node, just quit this loop so we can go
+			//print the transitions. Note that it maintains its postion
+			break;
+		}
+		++it;
+	}
+	if (it.complete())
+	{
+		//this means we traversed the entire tree and didn't find the goal. Either
+		//there is a bug, or the problem isn't possible (for example the
+		//cannibals outnumber the missionaries). Just let the user know.
+		fout << "There was no solution to this problem";
+	} else
+	{
+		fout << "Format: <missionaries,...,cannibals,...,boat> where 0=origin, 1=destination" << std::endl;
+		//print the path to the goal
+		//Note that this function actually searches for the path again. I did
+		//it this way so that I could show how the tree is traversed above, but
+		//for convenience I just use my path function to get the real path
+		std::vector<ion::TreeNode<RiverState>*> path = tree.GetPath(goal_state);
+		
+		//The path is returned in order from the goal to the root, so traverse it backwards to print it in the logical order
+		for (std::vector<ion::TreeNode<RiverState>*>::reverse_iterator node_it = path.rbegin(); node_it != path.rend(); ++node_it)
+		{
+			fout << (*node_it)->GetData() << std::endl;
+		}
+	}
+
+	fout.close();
 	return 0;
 }
