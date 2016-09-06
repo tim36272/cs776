70c224be53baa0bb7e11831b1eab7a3355aba304
Tim Sweet <timothy.l.sweet@gmail.com>
2016-09-05 19:11:54 -0700

    Update to use breadth-first instead of depth-first search and update gitignore to ignore Visual Studio's opendb files

--- app/cannibals/src/main.cpp
+++ app/cannibals/src/main.cpp
@@ -2,6 +2,7 @@
 #include "ionlib\net.h"
 #include "ionlib\tree.h"
 #include <vector>
+#include <fstream>
 
 #define BOAT_CAPACITY 2
 #define NUM_MISSIONARIES 3
@@ -25,6 +26,21 @@ public:
 		size_t num_missionaries_rhs = std::count(rhs.missionary_state.begin(), rhs.missionary_state.end(), false);
 		size_t num_canibals_rhs = std::count(rhs.cannibal_state.begin(), rhs.cannibal_state.end(), false);
 		return (num_missionaries_lhs == num_missionaries_rhs && num_canibals_lhs == num_canibals_rhs);
+	}
+	friend std::ostream& operator<<(std::ostream& output, RiverState state)
+	{
+		output << "<";
+		for (std::vector<bool>::iterator missionary = state.missionary_state.begin(); missionary != state.missionary_state.end(); ++missionary)
+		{
+			output << *missionary << ",";
+		}
+		for (std::vector<bool>::iterator cannibal = state.cannibal_state.begin(); cannibal != state.cannibal_state.end(); ++cannibal)
+		{
+			output << *cannibal << ",";
+		}
+		output << state.boat_state;
+		output << ">";
+		return output;
 	}
 };
 RiverState movePeople(RiverState state, size_t num_missionaries, size_t num_cannibals)
@@ -58,31 +74,21 @@ void enumerateAllStates(ion::TreeNode<RiverState>& root, ion::TreeNode<RiverStat
 {
 	LOGDEBUG("Descending");
 	//get the number of cannibals/missionaries that could possibly be moved
-	RiverState state = node->GetData();
-	size_t num_missionaries_movable = std::count(state.missionary_state.begin(), state.missionary_state.end(), state.boat_state);
-	size_t num_cannibals_movable = std::count(state.cannibal_state.begin(), state.cannibal_state.end(), state.boat_state);
-
 	//cap the number of movable people at BOAT_CAPACITY
-	num_missionaries_movable = min(num_missionaries_movable, BOAT_CAPACITY);
-	num_cannibals_movable = min(num_cannibals_movable, BOAT_CAPACITY);
+	RiverState state = node->GetData();
+	size_t num_missionaries_movable = min(BOAT_CAPACITY,std::count(state.missionary_state.begin(), state.missionary_state.end(), state.boat_state));
+	size_t num_cannibals_movable = min(BOAT_CAPACITY, std::count(state.cannibal_state.begin(), state.cannibal_state.end(), state.boat_state));
+	std::vector<ion::TreeNode<RiverState>*> pending_nodes;
+
 	//for node, generate all of its children.
 	for (size_t num_missionaries_moved = 0; num_missionaries_moved <= num_missionaries_movable; ++num_missionaries_moved)
 	{
 		size_t max_cannibals_to_move;
-		if (num_cannibals_movable < num_missionaries_moved)
-		{
-			max_cannibals_to_move = 0;
-		} else
-		{
-			max_cannibals_to_move = (num_cannibals_movable - num_missionaries_moved);
-		}
+		max_cannibals_to_move = (num_cannibals_movable < num_missionaries_moved) ? 0 : (num_cannibals_movable - num_missionaries_moved);
 		for (size_t num_cannibals_moved = 0; num_cannibals_moved <= max_cannibals_to_move; ++num_cannibals_moved)
 		{
-			if (num_cannibals_moved == 0 && num_missionaries_moved == 0)
-			{
-				//there is no one to drive the boat, skip this state
-				continue;
-			}
+			//check if there is no one to drive the boat, if so skip this state
+			if (num_cannibals_moved == 0 && num_missionaries_moved == 0) continue;
 			//note that the conditions on getting to this point in the loop guarantees that there are enough people to move and the boat will fit this many people
 			//so we can just generate the state and check if it is in the tree already
 			RiverState new_state = movePeople(state, num_missionaries_moved, num_cannibals_moved);
@@ -90,20 +96,17 @@ void enumerateAllStates(ion::TreeNode<RiverState>& root, ion::TreeNode<RiverStat
 			std::vector<ion::TreeNode<RiverState>*> path = root.GetPath(new_state);
 			if (path.empty())
 			{
-				LOGDEBUG("Adding: <%d,%d,%d><%d,%d,%d><%d>",
-						 int(new_state.missionary_state[0]),
-						 int(new_state.missionary_state[1]),
-						 int(new_state.missionary_state[2]),
-						 int(new_state.cannibal_state[0]),
-						 int(new_state.cannibal_state[1]),
-						 int(new_state.cannibal_state[2]),
-						 new_state.boat_state);
 				//there is no path in the tree which leads to an equivalent state, which means this state is novel
 				node->AddLeaf(new_state);
 				//enumerate all the children of that state
-				enumerateAllStates(root, node->GetLeaf(node->NumLeafs() - 1));
+				pending_nodes.push_back(node->GetLeaf(node->NumLeafs() - 1));
+				
 			}
 		}
+	}
+	for (std::vector<ion::TreeNode<RiverState>*>::iterator node_it = pending_nodes.begin(); node_it != pending_nodes.end(); ++node_it)
+	{
+		enumerateAllStates(root, *node_it);
 	}
 	LOGDEBUG("Ascending");
 }
@@ -119,6 +122,11 @@ int main(int argc, char* argv[])
 
 	ion::TreeNode<RiverState> tree(initial_state,nullptr);
 	enumerateAllStates(tree, &tree);
+	
+	std::ofstream file;
+	file.open("tree.gv");
+	tree.print(file);
+	file.close();
 
 	//ion::TreeNode<int>::iterator it(ion::DEPTH_FIRST);
 	//it.init(tree);
